ab725a847d289daddf797da887bd0093f71da4e4
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2019-12-04 15:26:58 -0300

    creates rename function

--- src/Library/Renamer.fs
+++ src/Library/Renamer.fs
@@ -7,6 +7,13 @@ let project files funcs =
     let getProjection (file:FileInfo) = (file.DirectoryName, file.Name, processName file.Name) 
     files |> List.map getProjection
 
+let rename files =
+    let move (dirName, oldName, newName) = 
+        let oldPath = Path.Combine(dirName, oldName)
+        let newPath = Path.Combine(dirName, newName)
+        File.Move(oldPath, newPath)
+    List.iter move files
+
 let getFiles dirName ext recursive =
     let searchType = if recursive then SearchOption.AllDirectories else SearchOption.TopDirectoryOnly
     Directory.GetFiles(dirName, "*." + ext, searchType)
