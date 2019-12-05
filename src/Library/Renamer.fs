31804013944d57300f048a3edd1862c4634f35b7
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2019-12-05 18:49:17 -0300

    improviemtns

--- src/Library/Renamer.fs
+++ src/Library/Renamer.fs
@@ -1,26 +1,31 @@
 module Renamer
 
 open System.IO
+open System.Linq
+open MoreLinq
 
 type FileConfiguration =
   { DirectoryName: string
     OldName: string
     NewName: string }
+    member this.OldPath = Path.Combine(this.DirectoryName, this.OldName)
+    member this.NewPath = Path.Combine(this.DirectoryName, this.NewName)
 
 let project files funcs =
-    let processName fileName = List.fold (fun name f -> f name) fileName funcs
-    let getProjection (file:FileInfo) = { DirectoryName = file.DirectoryName 
-                                          OldName = file.Name
-                                          NewName = processName file.Name }
+    let processName fileName = List.fold (|>) fileName funcs
+    let getProjection (file: FileInfo) = { DirectoryName = file.DirectoryName 
+                                           OldName = file.Name
+                                           NewName = processName file.Name }
     files |> List.map getProjection
 
 let rename files =
-    let move file = 
-        let oldPath = Path.Combine(file.DirectoryName, file.OldName)
-        let newPath = Path.Combine(file.DirectoryName, file.NewName)
-        File.Move(oldPath, newPath)
-    List.iter move files
+    List.iter 
+        (fun (file: FileConfiguration) -> File.Move(file.OldPath, file.NewPath))
+        files
 
+let hasNewNameConflicts files = 
+    List.length files <> MoreEnumerable.DistinctBy(files, fun f -> f.NewName).Count()
+    
 let getFiles dirName ext recursive =
     let searchType = if recursive then SearchOption.AllDirectories else SearchOption.TopDirectoryOnly
     Directory.GetFiles(dirName, "*." + ext, searchType)
