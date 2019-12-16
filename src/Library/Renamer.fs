1762934231e03ad65adbb09bfc52f4cc521f1e83
Leandro Fernandes Vieira <leandro.vieira@stone.com.br>
2019-12-16 13:08:12 -0300

    fix

--- src/Library/Renamer.fs
+++ src/Library/Renamer.fs
@@ -7,9 +7,17 @@ open MoreLinq
 type FileConfiguration =
   { DirectoryName: string
     OldName: string
-    NewName: string }
-    member this.OldPath = Path.Combine(this.DirectoryName, this.OldName)
-    member this.NewPath = Path.Combine(this.DirectoryName, this.NewName)
+    NewName: string
+    OldPath: string
+    NewPath: string }
+
+let createRecord dirName oldName newName  =
+    { DirectoryName = dirName
+      OldName = oldName
+      NewName = newName
+      OldPath = Path.Combine(dirName, oldName)
+      NewPath = Path.Combine(dirName, newName)
+    }
 
 let processFileName fileName funcs = 
     let name = Path.GetFileNameWithoutExtension(fileName)
@@ -18,18 +26,16 @@ let processFileName fileName funcs =
     newName + ext 
 
 let project (files: FileInfo list) funcs =
-    files |> List.map (fun f -> { DirectoryName = f.DirectoryName 
-                                  OldName = f.Name
-                                  NewName = processFileName f.Name funcs })
+    files |> List.map (fun f -> createRecord f.DirectoryName f.Name <| processFileName f.Name funcs)
 
 let private rename getNames =
     List.iter (getNames >> File.Move)
 
 let renameForward =
-    rename (fun f -> f.OldName, f.NewName)
+    rename (fun f -> f.OldPath, f.NewPath)
 
 let renameBackward =
-    rename (fun f -> f.NewName, f.OldName)
+    rename (fun f -> f.NewPath, f.OldPath)
 
 let hasNewNameConflicts files = 
     List.length files <> MoreEnumerable.DistinctBy(files, fun f -> f.NewName).Count()
